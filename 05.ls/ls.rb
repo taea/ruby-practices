21bd105e17e45b5a5c571627abdc781c36b0a7e1
taea <igiari@gmail.com>
2022-07-14 17:16:10 +0900

    Rubocop; Use ternary operator and .zero?

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -39,11 +39,7 @@ end
 current_dir = '.'
 column_number = 3
 files = find_files(current_dir)
-
-files_per_column = files.length / column_number
-unless files.length % column_number == 0
-  files_per_column += 1
-end
+files_per_column = (files.length % column_number).zero? ? files.length / column_number : files.length / column_number + 1
 max_length = max_length(files)
 columns = split_into_column(files, files_per_column)
 display_files(columns, files_per_column, column_number, max_length)
