e26416f79633763975878c48a2d2d469ad48fbc4
taea <igiari@gmail.com>
2022-07-15 02:04:45 +0900

    Use &: instead of block

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -21,6 +21,6 @@ current_dir = '.'
 columns_number = 3
 files = find_files(current_dir)
 rows_number = files.length / columns_number + ((files.length % columns_number).zero? ? 0 : 1)
-max_length = files.map { |item| item.length }.max
-columns_array = files.each_slice(rows_number).map { |file| file.to_a }
+max_length = files.map(&:length).max
+columns_array = files.each_slice(rows_number).map(&:to_a)
 display_files(columns_array, rows_number, columns_number, max_length)
