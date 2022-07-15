f6c792911bf1c160d410c2bf52a8f7a1678f81d0
taea <igiari@gmail.com>
2022-07-15 14:35:33 +0900

    Remove unecessary variable

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -19,5 +19,4 @@ files          = find_files(current_dir)
 columns_number = 3
 rows_number    = files.length / columns_number + ((files.length % columns_number).zero? ? 0 : 1)
 max_length     = files.map(&:length).max
-columns_array  = files.each_slice(rows_number).map(&:to_a)
-display_files(columns_array, rows_number, columns_number, max_length)
+display_files(files.each_slice(rows_number).map(&:to_a), rows_number, columns_number, max_length)
