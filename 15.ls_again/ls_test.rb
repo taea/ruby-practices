f76048ffeb71cc757f159888fdc310e7aab756d3
taea <igiari@gmail.com>
2023-01-21 07:56:55 +0900

    Rewrite test to make it more readable

--- 15.ls_again/ls_test.rb
+++ 15.ls_again/ls_test.rb
@@ -4,43 +4,91 @@ require 'minitest/autorun'
 require_relative 'ls'
 
 class LsTest < Minitest::Test
-  def test_1_file
-    assert_equal '1', show(%w[1], COLUMNS)
+  def test_1_files
+    expected = <<~EOS.chomp
+      1
+    EOS
+    actual = show(%w[1], COLUMNS)
+    assert_equal expected, actual
   end
 
   def test_2_files
-    assert_equal '1 2', show(%w[1 2], COLUMNS)
+    expected = <<~EOS.chomp
+      1 2
+    EOS
+    actual = show(%w[1 2], COLUMNS)
+    assert_equal expected, actual
   end
 
   def test_3_files
-    assert_equal '1 2 3', show(%w[1 2 3], COLUMNS)
+    expected = <<~EOS.chomp
+      1 2 3
+    EOS
+    actual = show(%w[1 2 3], COLUMNS)
+    assert_equal expected, actual
   end
 
   def test_4_files
-    assert_equal '1 3\n2 4', show(%w[1 2 3 4], COLUMNS)
+    expected = <<~EOS.chomp
+      1 2 3
+      4
+    EOS
+    actual = show(%w[1 2 3 4], COLUMNS)
+    assert_equal expected, actual
   end
 
   def test_5_files
-    assert_equal '1 3 5\n2 4', show(%w[1 2 3 4 5], COLUMNS)
+    expected = <<~EOS.chomp
+      1 3 5
+      2 4
+    EOS
+    actual = show(%w[1 2 3 4 5], COLUMNS)
+    assert_equal expected, actual
   end
 
   def test_6_files
-    assert_equal '1 3 5\n2 4 6', show(%w[1 2 3 4 5 6], COLUMNS)
+    expected = <<~EOS.chomp
+      1 3 5
+      2 4 6
+    EOS
+    actual = show(%w[1 2 3 4 5 6], COLUMNS)
+    assert_equal expected, actual
   end
 
   def test_7_files
-    assert_equal '1 4 7\n2 5\n3 6', show(%w[1 2 3 4 5 6 7], COLUMNS)
+    expected = <<~EOS.chomp
+      1 4 7
+      2 5
+      3 6
+    EOS
+    actual = show(%w[1 2 3 4 5 6 7], COLUMNS)
+    assert_equal expected, actual
   end
 
   def test_8_files
-    assert_equal '1 4 7\n2 5 8\n3 6', show(%w[1 2 3 4 5 6 7 8], COLUMNS)
+    expected = <<~EOS.chomp
+      1 4 7
+      2 5 8
+      3 6
+    EOS
+    actual = show(%w[1 2 3 4 5 6 7 8], COLUMNS)
+    assert_equal expected, actual
   end
 
   def test_various_file_names
-    assert_equal 'dir             gb              test-dir2\n' \
-      'esa             ls.rb           test-dir2のコピー\n' \
-      'folder          test-dir1       test-dir2のコピー2', show(
-        %w[dir esa folder gb ls.rb test-dir1 test-dir2 test-dir2のコピー test-dir2のコピー2], COLUMNS
-      )
+    expected = <<~EOS.chomp
+      dir             gb              test-dir2
+      esa             ls.rb           test-dir2のコピー
+      folder          test-dir1       test-dir2のコピー2
+    EOS
+    actual = show(
+      %w[
+        dir esa folder
+        gb ls.rb test-dir1
+        test-dir2 test-dir2のコピー test-dir2のコピー2
+      ],
+      COLUMNS
+    )
+    assert_equal expected, actual
   end
 end
