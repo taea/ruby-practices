46591fd8e2cfc6651b5c9e56bb393dd073dd8568
taea <igiari@gmail.com>
2023-01-20 14:32:18 +0900

    Remove code to make 2 columns when there were 4 files

--- 15.ls_again/ls.rb
+++ 15.ls_again/ls.rb
@@ -6,15 +6,7 @@ end
 
 def show(files, max_cols_count)
   rows_count = files.length / max_cols_count + ((files.length % max_cols_count).zero? ? 0 : 1)
-  cols_count =
-    case files.length
-    when max_cols_count + 1
-      max_cols_count - 1
-    when 0..max_cols_count
-      files.length
-    else
-      max_cols_count
-    end
+  cols_count = files.length < max_cols_count ? files.length : max_cols_count
   matrix     = files.each_slice(rows_count).to_a
   max_length = files.map(&:length).max
   rows_count.times do |row|
