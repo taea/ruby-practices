2fcfdcf33848f86647fc1cf9c793e1c3bf047522
taea <igiari@gmail.com>
2023-01-22 13:22:34 +0900

    Refactor; Use map

--- 15.ls_again/ls.rb
+++ 15.ls_again/ls.rb
@@ -3,16 +3,12 @@
 def filename(dir, max_cols_count)
   files      = Dir.foreach(dir).reject { |f| f.start_with?('.') }.sort
   rows_count = (files.length.to_f / max_cols_count).ceil
-  cols_count = files.length < max_cols_count ? files.length : max_cols_count
   matrix     = files.each_slice(rows_count).to_a
   max_length = files.map(&:length).max
   filenames  = []
   rows_count.times do |row|
-    rows_filenames = []
-    cols_count.times do |col|
-      rows_filenames << matrix[col][row]&.ljust(max_length + 1) unless matrix[col].nil?
-    end
-  filenames << rows_filenames.compact
+    rows_filenames = matrix.map { |matrix| matrix[row]&.ljust(max_length + 1) }
+    filenames << rows_filenames.compact
   end
   filenames
 end
