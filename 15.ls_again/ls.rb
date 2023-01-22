16e4380f323b106ae2a3b84840dd02bff5e64329
taea <igiari@gmail.com>
2023-01-22 09:24:20 +0900

    Split method between sort and display

--- 15.ls_again/ls.rb
+++ 15.ls_again/ls.rb
@@ -1,15 +1,25 @@
 # frozen_string_literal: true
 
-def show(dir, max_cols_count)
+def filename(dir, max_cols_count)
   files      = Dir.foreach(dir).reject { |f| f.start_with?('.') }.sort
   rows_count = (files.length.to_f / max_cols_count).ceil
   cols_count = files.length < max_cols_count ? files.length : max_cols_count
   matrix     = files.each_slice(rows_count).to_a
   max_length = files.map(&:length).max
+  filenames  = []
   rows_count.times do |row|
+    rows_filenames = []
     cols_count.times do |col|
-      print matrix[col][row]&.ljust(max_length + 1) unless matrix[col].nil?
+      rows_filenames << matrix[col][row]&.ljust(max_length + 1) unless matrix[col].nil?
     end
+  filenames << rows_filenames.compact
+  end
+  filenames
+end
+
+def show(filenames)
+  filenames.each do |row|
+    row.each { |filename| print filename }
     print "\n"
   end
 end
@@ -17,4 +27,5 @@ end
 COLUMNS = 3
 
 target = ARGV.empty? ? '.' : ARGV[0]
-show(target, COLUMNS)
+filenames = filename(target, COLUMNS)
+show(filenames)
