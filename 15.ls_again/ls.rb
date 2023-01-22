c77e2c9bc3a07e4fae4c966b1ce7a9aa53468d75
taea <igiari@gmail.com>
2023-01-22 17:50:25 +0900

    Use Enumerable#map to make filenames array

--- 15.ls_again/ls.rb
+++ 15.ls_again/ls.rb
@@ -5,12 +5,9 @@ def filename(dir, max_cols_count)
   rows_count = (files.length.to_f / max_cols_count).ceil
   matrix     = files.each_slice(rows_count).to_a
   max_length = files.map(&:length).max
-  filenames  = []
-  rows_count.times do |row|
-    rows_filenames = matrix.map { |cols_filenames| cols_filenames[row]&.ljust(max_length + 1) }.join.rstrip
-    filenames << rows_filenames
+  rows_count.times.map do |row|
+    matrix.map { |cols_filenames| cols_filenames[row]&.ljust(max_length + 1) }.join.rstrip
   end
-  filenames
 end
 
 def show(filenames)
