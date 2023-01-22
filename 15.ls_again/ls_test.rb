2864338ad118338ad8a6b4d96e08276e7f3ef0d5
taea <igiari@gmail.com>
2023-01-22 15:51:52 +0900

    Rewrite test with assert_output to make it work

--- 15.ls_again/ls_test.rb
+++ 15.ls_again/ls_test.rb
@@ -2,93 +2,98 @@
 
 require 'minitest/autorun'
 require_relative 'ls'
+TEST_DIR = '../../ls-sample/'
+COLUMNS  = 3
 
 class LsTest < Minitest::Test
+
   def test_1_files
+    dir = TEST_DIR + '1'
     expected = <<~TEXT.chomp
       1
     TEXT
-    actual = show(%w[1], COLUMNS)
-    assert_equal expected, actual
+    filenames  = filename(dir, COLUMNS)
+    assert_output(expected) { show(filenames) }
   end
 
   def test_2_files
+    dir = TEST_DIR + '2'
     expected = <<~TEXT.chomp
       1 2
     TEXT
-    actual = show(%w[1 2], COLUMNS)
-    assert_equal expected, actual
+    filenames  = filename(dir, COLUMNS)
+    assert_output(expected) { show(filenames) }
   end
 
   def test_3_files
+    dir = TEST_DIR + '3'
     expected = <<~TEXT.chomp
       1 2 3
     TEXT
-    actual = show(%w[1 2 3], COLUMNS)
-    assert_equal expected, actual
+    filenames  = filename(dir, COLUMNS)
+    assert_output(expected) { show(filenames) }
   end
 
   def test_4_files
+    dir = TEST_DIR + '4'
     expected = <<~TEXT.chomp
-      1 2 3
-      4
+      1 3
+      2 4
     TEXT
-    actual = show(%w[1 2 3 4], COLUMNS)
-    assert_equal expected, actual
+    filenames = filename(dir, COLUMNS)
+    assert_output(expected) { show(filenames) }
   end
 
   def test_5_files
+    dir = TEST_DIR + '5'
     expected = <<~TEXT.chomp
       1 3 5
       2 4
     TEXT
-    actual = show(%w[1 2 3 4 5], COLUMNS)
-    assert_equal expected, actual
+    filenames  = filename(dir, COLUMNS)
+    assert_output(expected) { show(filenames) }
   end
 
   def test_6_files
+    dir = TEST_DIR + '6'
     expected = <<~TEXT.chomp
       1 3 5
       2 4 6
     TEXT
-    actual = show(%w[1 2 3 4 5 6], COLUMNS)
-    assert_equal expected, actual
+    filenames  = filename(dir, COLUMNS)
+    assert_output(expected) { show(filenames) }
   end
 
   def test_7_files
+    dir = TEST_DIR + '7'
     expected = <<~TEXT.chomp
       1 4 7
       2 5
       3 6
     TEXT
-    actual = show(%w[1 2 3 4 5 6 7], COLUMNS)
-    assert_equal expected, actual
+    filenames  = filename(dir, COLUMNS)
+    assert_output(expected) { show(filenames) }
   end
 
   def test_8_files
+    dir = TEST_DIR + '8'
     expected = <<~TEXT.chomp
       1 4 7
       2 5 8
       3 6
     TEXT
-    actual = show(%w[1 2 3 4 5 6 7 8], COLUMNS)
-    assert_equal expected, actual
+    filenames  = filename(dir, COLUMNS)
+    assert_output(expected) { show(filenames) }
   end
 
   def test_various_file_names
+    dir = TEST_DIR + 'long_file_name'
     expected = <<~TEXT.chomp
       dir             gb              test-dir2
       esa             ls.rb           test-dir2のコピー
       folder          test-dir1       test-dir2のコピー2
     TEXT
-    actual = show(
-      %w[
-        dir esa folder
-        gb ls.rb test-dir1
-        test-dir2 test-dir2のコピー test-dir2のコピー2
-      ],
-      COLUMNS
-    )
-    assert_equal expected, actual
+    filenames  = filename(dir, COLUMNS)
+    assert_output(expected) { show(filenames) }
   end
 end
