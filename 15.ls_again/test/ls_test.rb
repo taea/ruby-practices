287d2f1b951c9c14f6f5a1eda99a14ac6b711e7d
taea <igiari@gmail.com>
2023-01-28 18:05:40 +0900

    Avoid self-joining pathname

--- 15.ls_again/test/ls_test.rb
+++ 15.ls_again/test/ls_test.rb
@@ -2,11 +2,11 @@
 
 require 'minitest/autorun'
 require_relative '../lib/ls'
-TEST_DIR = 'ls-sample/'
+TEST_DIR = 'ls-sample'
 
 class LsTest < Minitest::Test
   def test_1_files
-    dir = "#{TEST_DIR}1"
+    dir = File.join(TEST_DIR, '1')
     expected = <<~TEXT.chomp
       1
     TEXT
@@ -14,7 +14,7 @@ class LsTest < Minitest::Test
   end
 
   def test_2_files
-    dir = "#{TEST_DIR}2"
+    dir = File.join(TEST_DIR, '2')
     expected = <<~TEXT.chomp
       1 2
     TEXT
@@ -22,7 +22,7 @@ class LsTest < Minitest::Test
   end
 
   def test_3_files
-    dir = "#{TEST_DIR}3"
+    dir = File.join(TEST_DIR, '3')
     expected = <<~TEXT.chomp
       1 2 3
     TEXT
@@ -30,7 +30,7 @@ class LsTest < Minitest::Test
   end
 
   def test_4_files
-    dir = "#{TEST_DIR}4"
+    dir = File.join(TEST_DIR, '4')
     expected = <<~TEXT.chomp
       1 3
       2 4
@@ -39,7 +39,7 @@ class LsTest < Minitest::Test
   end
 
   def test_5_files
-    dir = "#{TEST_DIR}5"
+    dir = File.join(TEST_DIR, '5')
     expected = <<~TEXT.chomp
       1 3 5
       2 4
@@ -48,7 +48,7 @@ class LsTest < Minitest::Test
   end
 
   def test_6_files
-    dir = "#{TEST_DIR}6"
+    dir = File.join(TEST_DIR, '6')
     expected = <<~TEXT.chomp
       1 3 5
       2 4 6
@@ -57,7 +57,7 @@ class LsTest < Minitest::Test
   end
 
   def test_7_files
-    dir = "#{TEST_DIR}7"
+    dir = File.join(TEST_DIR, '7')
     expected = <<~TEXT.chomp
       1 4 7
       2 5
@@ -67,7 +67,7 @@ class LsTest < Minitest::Test
   end
 
   def test_8_files
-    dir = "#{TEST_DIR}8"
+    dir = File.join(TEST_DIR, '8')
     expected = <<~TEXT.chomp
       1 4 7
       2 5 8
@@ -77,7 +77,7 @@ class LsTest < Minitest::Test
   end
 
   def test_various_file_names
-    dir = "#{TEST_DIR}long_file_name"
+    dir = File.join(TEST_DIR, 'long_file_name')
     expected = <<~TEXT.chomp
       dir             gb              test-dir2
       esa             ls.rb           test-dir2のコピー
