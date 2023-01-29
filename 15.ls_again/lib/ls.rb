911dab50bc563d7208be24dab9b45e8482a15f5a
taea <igiari@gmail.com>
2023-01-30 07:53:27 +0900

    Align position of blocks assined variables with `=`

--- 15.ls_again/lib/ls.rb
+++ 15.ls_again/lib/ls.rb
@@ -5,20 +5,20 @@ def find(dir)
 end
 
 def sort(files, cols_count)
-  rows_count = (files.length.to_f / cols_count).ceil
-  col_matrix = files.each_slice(rows_count).to_a
-  max_length = files.map(&:length).max
-  row_matrix = Array.new(rows_count) do |row|
-    col_matrix.map do |col_arrays|
-      col_arrays[row]
-    end
-  end
+  rows_count    = (files.length.to_f / cols_count).ceil
+  col_matrix    = files.each_slice(rows_count).to_a
+  max_length    = files.map(&:length).max
+  row_matrix    = Array.new(rows_count) do |row|
+                    col_matrix.map do |col_arrays|
+                      col_arrays[row]
+                    end
+                  end
   formatted_row = row_matrix.map do |row_files|
-    row_files.map do |file|
-      file&.ljust(max_length + 1)
-    end
-  end
-  joined_row = formatted_row.map(&:join)
+                    row_files.map do |file|
+                      file&.ljust(max_length + 1)
+                    end
+                  end
+  joined_row    = formatted_row.map(&:join)
   joined_row.map(&:rstrip)
 end
 
