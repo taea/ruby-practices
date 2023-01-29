dfb087a8f98fbd70663b3718fe0d0eb457a68840
taea <igiari@gmail.com>
2023-01-30 07:19:27 +0900

    s/dir/target/; Match program and test variables"

--- 15.ls_again/test/ls_test.rb
+++ 15.ls_again/test/ls_test.rb
@@ -6,83 +6,83 @@ TEST_DIR = 'ls-sample'
 
 class LsTest < Minitest::Test
   def test_1_files
-    dir = File.join(TEST_DIR, '1')
+    target = File.join(TEST_DIR, '1')
     expected = <<~TEXT.chomp
       1
     TEXT
-    assert_output(expected) { display(filenames(dir, COLUMNS)) }
+    assert_output(expected) { display(filenames(target, COLUMNS)) }
   end
 
   def test_2_files
-    dir = File.join(TEST_DIR, '2')
+    target = File.join(TEST_DIR, '2')
     expected = <<~TEXT.chomp
       1 2
     TEXT
-    assert_output(expected) { display(filenames(dir, COLUMNS)) }
+    assert_output(expected) { display(filenames(target, COLUMNS)) }
   end
 
   def test_3_files
-    dir = File.join(TEST_DIR, '3')
+    target = File.join(TEST_DIR, '3')
     expected = <<~TEXT.chomp
       1 2 3
     TEXT
-    assert_output(expected) { display(filenames(dir, COLUMNS)) }
+    assert_output(expected) { display(filenames(target, COLUMNS)) }
   end
 
   def test_4_files
-    dir = File.join(TEST_DIR, '4')
+    target = File.join(TEST_DIR, '4')
     expected = <<~TEXT.chomp
       1 3
       2 4
     TEXT
-    assert_output(expected) { display(filenames(dir, COLUMNS)) }
+    assert_output(expected) { display(filenames(target, COLUMNS)) }
   end
 
   def test_5_files
-    dir = File.join(TEST_DIR, '5')
+    target = File.join(TEST_DIR, '5')
     expected = <<~TEXT.chomp
       1 3 5
       2 4
     TEXT
-    assert_output(expected) { display(filenames(dir, COLUMNS)) }
+    assert_output(expected) { display(filenames(target, COLUMNS)) }
   end
 
   def test_6_files
-    dir = File.join(TEST_DIR, '6')
+    target = File.join(TEST_DIR, '6')
     expected = <<~TEXT.chomp
       1 3 5
       2 4 6
     TEXT
-    assert_output(expected) { display(filenames(dir, COLUMNS)) }
+    assert_output(expected) { display(filenames(target, COLUMNS)) }
   end
 
   def test_7_files
-    dir = File.join(TEST_DIR, '7')
+    target = File.join(TEST_DIR, '7')
     expected = <<~TEXT.chomp
       1 4 7
       2 5
       3 6
     TEXT
-    assert_output(expected) { display(filenames(dir, COLUMNS)) }
+    assert_output(expected) { display(filenames(target, COLUMNS)) }
   end
 
   def test_8_files
-    dir = File.join(TEST_DIR, '8')
+    target = File.join(TEST_DIR, '8')
     expected = <<~TEXT.chomp
       1 4 7
       2 5 8
       3 6
     TEXT
-    assert_output(expected) { display(filenames(dir, COLUMNS)) }
+    assert_output(expected) { display(filenames(target, COLUMNS)) }
   end
 
   def test_various_file_names
-    dir = File.join(TEST_DIR, 'long_file_name')
+    target = File.join(TEST_DIR, 'long_file_name')
     expected = <<~TEXT.chomp
       dir             gb              test-dir2
       esa             ls.rb           test-dir2のコピー
       folder          test-dir1       test-dir2のコピー2
     TEXT
-    assert_output(expected) { display(filenames(dir, COLUMNS)) }
+    assert_output(expected) { display(filenames(target, COLUMNS)) }
   end
 end
